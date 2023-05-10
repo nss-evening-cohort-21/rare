c9524bfb6b0525629421df7b5bc8b49e350c6015
ryanmbigelow <ryanmbigelow@gmail.com>
2023-05-09 20:26:32 -0500

    added response, parsed, resource, id to do_get

--- request_handler.py
+++ request_handler.py
@@ -1,5 +1,6 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
+from urllib.parse import urlparse, parse_qs
 
 from views.user import create_user, login_user
 
@@ -51,8 +52,16 @@ class HandleRequests(BaseHTTPRequestHandler):
 
     def do_GET(self):
         """Handle Get requests to the server"""
-        pass
+        self._set_headers(200)
 
+        response = {}
+
+        # Parse URL and store entire tuple in a variable
+        parsed = self.parse_url(self.path)
+
+        # If the path does not include a query parameter, continue with the original if block
+        if '?' not in self.path:
+            ( resource, id ) = parsed
 
     def do_POST(self):
         """Make a post request to the server"""
