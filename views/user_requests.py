9517ccee34351a4a7a05a2fed9b6540a9e341df4
Allison Blumenthal <blumen.allison@gmail.com>
2023-05-11 19:20:03 -0400

    wrote get_user_by_first_name function

--- views/user_requests.py
+++ views/user_requests.py
@@ -146,3 +146,44 @@ def get_single_user(id):
         user = User(data['id'], data['first_name'], data['last_name'], data['email'], data['bio'], data['username'], data['password'], data['profile_image_url'], data['created_on'], data['active'])
         
         return user.__dict__
+    
+def search_user_by_first_name(first_name):
+    """Searches users by first name
+
+    Args:
+    first_name (string): The string passed to the get request
+
+    Returns:
+        A list of dictionaries with matching user first names
+    """
+    with sqlite3.connect('./db.sqlite3') as conn:
+        
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+        
+        db_cursor.execute("""
+        SELECT
+            u.id, 
+            u.first_name, 
+            u.last_name,
+            u.email, 
+            u.bio,
+            u.username,
+            u.password,
+            u.profile_image_url,
+            u.created_on,
+            u.active
+        FROM Users u
+        WHERE u.first_name = ?
+        """, ( first_name, ))
+        
+        users = []
+        dataset = db_cursor.fetchall()
+        
+        for row in dataset:
+            
+            user = User(row['id'], row['first_name'], row['last_name'], row['email'], row['bio'], row['username'], row['password'], row['profile_image_url'], row['created_on'], row['active'])
+            
+            users.append(user.__dict__)
+        
+    return users
