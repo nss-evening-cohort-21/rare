28a8f7ed3ffea115457e0773e9ccd408d6f80553
Allison Blumenthal <blumen.allison@gmail.com>
2023-05-11 21:41:35 -0400

    added update function

--- views/post_requests.py
+++ views/post_requests.py
@@ -75,3 +75,27 @@ def delete_post(id):
         DELETE FROM Posts
         WHERE id = ?
         """, (id, ))
+
+def update_post(id, new_post):
+    with sqlite3.connect("./db.sqlite3") as conn:
+        db_cursor = conn.cursor()
+        db_cursor.execute("""
+        UPDATE Posts
+            SET
+                user_id = ?,
+                category_id = ?,
+                title = ?,
+                publication_date = ?,
+                image_url = ?,
+                content = ?,
+                approved = ?
+        WHERE id = ?
+        """, (new_post['user_id'], new_post['category_id'], new_post['title'], new_post['publication_date'], new_post['image_url'], new_post['content'], new_post['approved'], id, ))
+        
+        rows_affected = db_cursor.rowcount
+    
+    if rows_affected == 0:
+        return False
+    else:
+        return True
+    
